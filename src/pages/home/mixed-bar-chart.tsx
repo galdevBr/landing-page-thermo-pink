1a4082a5319e1a2b271c3596b97f3684465d08e0
Diego Galvao <di.galvao89@gmail.com>
2025-05-31 17:46:39 -0300

    Adds framer-motion and react-intersection-observer to mixed-bar-chart component

--- src/pages/home/mixed-bar-chart.tsx
+++ src/pages/home/mixed-bar-chart.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 import {
@@ -26,48 +28,65 @@ const chartConfig = {
 } satisfies ChartConfig
 
 export function BarChartComponent() {
+  const { ref, inView } = useInView({
+    threshold: 0.2, // Trigger when element is 20% visible
+    triggerOnce: true,
+  })
+
   return (
-    <div className="w-full h-full relative">
+    <div
+      className="w-full h-full relative"
+      ref={ref}
+    >
 
-      {/* //TODO: FIX ABSOLUTE POSITION */}
-      <div className="absolute right-22 translate-x-1/2 -translate-y-1/2 flex bottom-5 gap-2 text-white"> 
-        <ArrowBigUp className="h-10 w-10 md:h-12 md:w-12 text-white" fill="white" />
-        <p className="text-4xl md:text-5xl font-sans font-bold text-white">33%</p>
-      </div>
+      {inView && (
+        <>
+          {/* //TODO: FIX ABSOLUTE POSITION */}
+          <motion.div 
+            className="absolute right-22 translate-x-1/2 -translate-y-1/2 flex bottom-5 gap-2 text-white animation"
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.8 }}
+          >
+            <ArrowBigUp className="h-10 w-10 md:h-12 md:w-12 text-white" fill="white" />
+            <p className="text-4xl md:text-5xl font-sans font-bold text-white">33%</p>
+          </motion.div>
 
-      <ChartContainer config={chartConfig}>
-        <BarChart
-          accessibilityLayer
-          data={chartData}
-          layout="horizontal"
-          margin={{
-            left: 0,
-          }}
-          barCategoryGap={'20%'}
-        >
-          <XAxis
-            dataKey="vendor"
-            type="category"
-            tickLine={false}
-            tickMargin={10}
-            tickFormatter={(value) => 
-              chartConfig[value as keyof typeof chartConfig]?.label
-            }
-            padding={{
-              left: 0,
-              right: 148,
-            }}
-            tick={{ 
-              fontSize: 18,
-              fontWeight: 600,
-            }}
-          />
+          <ChartContainer config={chartConfig}>
+            <BarChart
+              accessibilityLayer
+              data={chartData}
+              layout="horizontal"
+              margin={{
+                left: 0,
+              }}
+              barCategoryGap={'20%'}
+            >
+              <XAxis
+                dataKey="vendor"
+                type="category"
+                tickLine={false}
+                tickMargin={10}
+                tickFormatter={(value) =>
+                  chartConfig[value as keyof typeof chartConfig]?.label
+                }
+                padding={{
+                  left: 0,
+                  right: 148,
+                }}
+                tick={{
+                  fontSize: 18,
+                  fontWeight: 600,
+                }}
+              />
 
-          <YAxis dataKey="porcentagem" type="number" domain={[0, 100]} />
+              <YAxis dataKey="porcentagem" type="number" domain={[0, 100]} />
 
-          <Bar dataKey="porcentagem" layout="horizontal" radius={[5, 5, 0, 0]} />
-        </BarChart>
-      </ChartContainer>
-    </div>
+              <Bar dataKey="porcentagem" layout="horizontal" radius={[5, 5, 0, 0]} animationDuration={1000} />
+            </BarChart>
+          </ChartContainer>
+        </>
+      )}
+    </div >
   )
 }
