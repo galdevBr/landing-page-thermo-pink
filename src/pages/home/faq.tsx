2828a5e49edf3ae93567f03032d329b11793c05e
Diego Galvao <di.galvao89@gmail.com>
2025-09-16 21:04:57 -0300

    Fixes issues on visual element from another section above accordion

--- src/pages/home/faq.tsx
+++ src/pages/home/faq.tsx
@@ -4,7 +4,7 @@ import { faqList } from "@/constants/faq-list";
 
 export function FAQSection() {
   return (
-    <section className="w-full py-16 px-4 md:px-8 lg:px-16 bg-pink-400" id="faq">
+    <section className="w-full py-16 px-4 md:px-8 lg:px-16 bg-pink-400 z-10" id="faq">
       <div className="container mx-auto max-w-5xl">
         <p className="text-3xl md:text-6xl font-bold text-black mb-12 text-center italic">PERGUNTAS FREQUENTES</p>
 
@@ -23,7 +23,7 @@ export function FAQSection() {
             className="bg-black hover:bg-black/80 cursor-pointer"
             asChild
           >
-            <a href='https://wa.me/5515997475220?text=Olá%2C+tenho+interesse+em+saber+mais+sobre+o+produto'>
+            <a href='https://wa.me/5515997475220/?text=Olá%2C+tenho+interesse+em+saber+mais+sobre+o+produto' target="_blank" rel="noopener noreferrer">
               FALE CONOSCO
             </a>
           </Button>
